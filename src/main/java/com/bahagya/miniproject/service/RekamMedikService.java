a7c64ca247377e491d7f0a428ef2cd74ee66714a
DESKTOP-I7DKLCJ\arisyamb <arsymnb@gmail.com>
2020-09-22 00:34:21 +0700

    Add assembler service controller rekam medik

---
+++ src/main/java/com/bahagya/miniproject/service/RekamMedikService.java
@@ -0,0 +1,7 @@
+package com.bahagya.miniproject.service;
+
+import com.bahagya.miniproject.model.dto.RekamMedikDto;
+
+public interface RekamMedikService {
+    RekamMedikDto insertRekamMedik(RekamMedikDto dto);
+}
