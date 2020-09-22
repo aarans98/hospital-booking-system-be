fdd6d58786ff9321f427238923ec0f8069c3011a
arnsumantri <62651191+rnsumantri@users.noreply.github.com>
2020-09-22 16:25:08 +0700

    add register add Entity Praktek (bugs) add Entity Dokter

---
+++ src/main/java/com/bahagya/miniproject/model/entity/Praktek.java
@@ -0,0 +1,29 @@
+
+package com.bahagya.miniproject.model.entity;
+
+import java.sql.Date;
+
+import javax.persistence.*;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "praktek")
+public class Praktek {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_praktek", nullable = false)
+    private Integer idPraktek;
+
+    @Column(name = "poli", nullable = false)
+    private String poli;
+
+    @Column(name = "jadwal", nullable = false)
+    private Date jadwal;
+
+//    @OneToMany
+//    @JoinColumn(name = "id_dokter", nullable = false, updatable = false)
+//    private Dokter idDokter;
+
+}
