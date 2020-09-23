8df861ea0c4d52a352d799fc6df1250adede353e
DESKTOP-I7DKLCJ\arisyamb <arsymnb@gmail.com>
2020-09-23 08:23:16 +0700

    add entity, repo, assembler for third table rekam_medik-obat

---
+++ src/main/java/com/bahagya/miniproject/repository/RmObatRepository.java
@@ -0,0 +1,8 @@
+package com.bahagya.miniproject.repository;
+
+import com.bahagya.miniproject.model.entity.RmObat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RmObatRepository extends JpaRepository<RmObat, Integer> {
+
+}
