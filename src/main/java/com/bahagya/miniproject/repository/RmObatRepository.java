bee781efd8f5a9dfe13e35bcaa2ca722e9bb9057
DESKTOP-I7DKLCJ\arisyamb <arsymnb@gmail.com>
2020-09-24 13:06:32 +0700

    add find all by RekamMedikIdRekamMedik

--- src/main/java/com/bahagya/miniproject/repository/RmObatRepository.java
+++ src/main/java/com/bahagya/miniproject/repository/RmObatRepository.java
@@ -1,8 +1,12 @@
 package com.bahagya.miniproject.repository;
 
+import java.util.List;
+
 import com.bahagya.miniproject.model.entity.RmObat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RmObatRepository extends JpaRepository<RmObat, Integer> {
+
+    List<RmObat> findAllByRekamMedikIdRekamMedik(Integer idRekamMedik);
 
 }
