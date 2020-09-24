bcd59e540acf98216910d54db2f33ffc6e507cf0
DESKTOP-I7DKLCJ\arisyamb <arsymnb@gmail.com>
2020-09-24 21:49:03 +0700

    fix repo jadwaldokter

--- src/main/java/com/bahagya/miniproject/repository/JadwalDokterRepo.java
+++ src/main/java/com/bahagya/miniproject/repository/JadwalDokterRepo.java
@@ -3,9 +3,14 @@ package com.bahagya.miniproject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import com.bahagya.miniproject.model.entity.JadwalDokter;
 
 @Repository
 public interface JadwalDokterRepo extends JpaRepository<JadwalDokter, Integer> {
 
+    List<JadwalDokter> findAllByDokterIdDokter(Integer idDokter);
+
+    List<JadwalDokter> findAllByPraktekIdPraktek(Integer idPraktek);
 }
