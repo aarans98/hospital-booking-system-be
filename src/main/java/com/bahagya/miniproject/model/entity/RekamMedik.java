483a3453bd0037eca665c57a261385c34d9cc843
DESKTOP-I7DKLCJ\arisyamb <arsymnb@gmail.com>
2020-09-24 08:41:12 +0700

    fix entity rekam medik, delete praktek repository

--- src/main/java/com/bahagya/miniproject/model/entity/RekamMedik.java
+++ src/main/java/com/bahagya/miniproject/model/entity/RekamMedik.java
@@ -23,22 +23,22 @@ public class RekamMedik {
     @Column(name = "id_pasien", nullable = false)
     private Integer idPasien;
 
-    // @ManyToOne
-    // @JoinColumn(name = "id_dokter", insertable = false, updatable = false)
-    // private Dokter dokter;
+    @ManyToOne
+    @JoinColumn(name = "id_dokter", insertable = false, updatable = false)
+    private Dokter dokter;
 
-    // @Column(name = "id_dokter", nullable = false)
-    // private Integer idDokter;
+    @Column(name = "id_dokter", nullable = false)
+    private Integer idDokter;
 
     @Column(name = "gejala")
     private String gejala;
 
-    // @OneToMany
-    // @JoinColumn(name = "id_praktek", insertable = false, updatable = false)
-    // private Set<Praktek> praktek;
+    @OneToOne
+    @JoinColumn(name = "id_praktek", insertable = false, updatable = false)
+    private Praktek praktek;
 
-    // @Column(name = "id_praktek", nullable = false)
-    // private Integer idPraktek;
+    @Column(name = "id_praktek", nullable = false)
+    private Integer idPraktek;
 
     @Column(name = "tanggal_kunjungan")
     private Date tanggalKunjungan;
@@ -51,9 +51,6 @@ public class RekamMedik {
 
     @Column(name = "diagnosa")
     private String diagnosa;
-
-    // @ManyToMany(mappedBy = "rekamMedik", cascade = CascadeType.ALL)
-    // private List<Obat> obat = new ArrayList<>();
 
     @OneToMany(mappedBy = "rekamMedik")
     private List<RmObat> rmObat;
