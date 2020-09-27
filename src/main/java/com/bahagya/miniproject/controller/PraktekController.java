5bf8cf75d20f92f6fb38c065b78a1bb154bf1d4b
gustianiarum <gustianiarumpramesti@gmail.com>
2020-09-27 16:41:17 +0700

    +add login

--- src/main/java/com/bahagya/miniproject/controller/PraktekController.java
+++ src/main/java/com/bahagya/miniproject/controller/PraktekController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.bahagya.miniproject.assembler.PraktekAssembler;
 import com.bahagya.miniproject.configuration.DefaultResponse;
@@ -18,6 +19,7 @@ import com.bahagya.miniproject.model.dto.PraktekDto;
 import com.bahagya.miniproject.model.entity.Praktek;
 import com.bahagya.miniproject.repository.PraktekRepo;
 
+@CrossOrigin(origins = "http://localhost:3001")
 @RestController
 @RequestMapping("/praktek")
 public class PraktekController {
@@ -35,12 +37,27 @@ public class PraktekController {
 				.collect(Collectors.toList());
 		return DefaultResponse.ok(praktekDtoList);
 	}
+	@GetMapping
+	public DefaultResponse getAll() {
+		List<Praktek> praktekList = repository.findAll();
+		List<PraktekDto> praktekDtoList = praktekList.stream().map(praktek -> assembler.fromEntity(praktek))
+				.collect(Collectors.toList());
+		return DefaultResponse.ok(praktekDtoList);
+	}
 
 	// http://localhost:1212/praktek/1
 	@GetMapping("/{id_praktek}")
 	public DefaultResponse get(@PathVariable Integer id_praktek) {
 		PraktekDto praktekDto = assembler.fromEntity(repository.findById(id_praktek).get());
 		return DefaultResponse.ok(praktekDto);
+	}
+
+	@GetMapping("/dokter/{id_dokter}")
+	public DefaultResponse getByDokter(@PathVariable Integer id_dokter) {
+		List<Praktek> praktekList = repository.findAllByDokterIdDokter(id_dokter);
+		List<PraktekDto> praktekDtoList = praktekList.stream().map(praktek -> assembler.fromEntity(praktek))
+				.collect(Collectors.toList());
+		return DefaultResponse.ok(praktekDtoList);
 	}
 
 	// http://localhost:1212/praktek
