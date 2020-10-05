5db04132c48d09766c98089ec420a716412b2ab5
DESKTOP-I7DKLCJ\arisyamb <arsymnb@gmail.com>
2020-10-05 18:32:34 +0700

    fix service rekam medik

--- src/main/java/com/bahagya/miniproject/service/RekamMedikServiceImpl.java
+++ src/main/java/com/bahagya/miniproject/service/RekamMedikServiceImpl.java
@@ -2,11 +2,15 @@ package com.bahagya.miniproject.service;
 
 import com.bahagya.miniproject.repository.RekamMedikRepository;
 import com.bahagya.miniproject.repository.RmObatRepository;
+import com.bahagya.miniproject.repository.ObatRepository;
 import com.bahagya.miniproject.assembler.FormRmAssembler;
 import com.bahagya.miniproject.assembler.RmObatAssembler;
 import com.bahagya.miniproject.model.dto.FormRmDto;
 import com.bahagya.miniproject.model.entity.RekamMedik;
 import com.bahagya.miniproject.model.entity.RmObat;
+import com.bahagya.miniproject.model.entity.Obat;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,14 +28,23 @@ public class RekamMedikServiceImpl implements RekamMedikService {
     private RmObatAssembler rmObatAssambler;
     @Autowired
     private RmObatRepository rmObatRepository;
+    @Autowired
+    private ObatRepository obatRepository;
 
     @Override
     public FormRmDto insertRekamMedik(FormRmDto dto) {
         RekamMedik entity = repository.save(assembler.fromDto(dto));
         repository.save(entity);
-        if (!dto.getIdObat().isEmpty()){
-            for(int i = 0; i < dto.getIdObat().size(); i++){
-                RmObat rmObat = rmObatAssambler.toEntity(dto.getId(), dto.getIdObat().get(i));
+        if (!dto.getNamaObat().isEmpty()) {
+            List<RmObat> temp = rmObatRepository.findAllByRekamMedikIdRekamMedik(dto.getId());
+            if (!temp.isEmpty()) {
+                for (int i = 0; i < temp.size(); i++) {
+                    rmObatRepository.deleteById(temp.get(i).getIdRmObat());
+                }
+            }
+            for (int i = 0; i < dto.getNamaObat().size(); i++) {
+                Obat obat = obatRepository.findByNamaObat(dto.getNamaObat().get(i)).get();
+                RmObat rmObat = rmObatAssambler.toEntity(dto.getId(), obat.getIdObat());
                 rmObatRepository.save(rmObat);
             }
         }
